3f88f9b1b61b74720f4477a6431ab854f2c6b516
kbwo <kabaaa1126@gmail.com>
2024-07-15 19:07:44 +0900

    add vitest demo

---
+++ test_proj/vitest/basic.test.ts
@@ -0,0 +1,12 @@
+import assert from "assert";
+import { describe, test } from "vitest";
+
+describe("describe text", () => {
+  test("pass", async () => {
+    assert(false);
+  });
+
+  test("fail", async () => {
+    assert(false);
+  });
+});
