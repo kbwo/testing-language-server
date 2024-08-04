90be4e498f92a393bcf121b9995e9b2d8162caff
kbwo <kabaaa1126@gmail.com>
2024-08-04 17:33:19 +0900

    chore(adapter): change log file location

--- crates/adapter/src/log.rs
+++ crates/adapter/src/log.rs
@@ -5,7 +5,7 @@ pub struct Log;
 impl Log {
     pub fn init() -> Result<WorkerGuard, anyhow::Error> {
         let home_dir = dirs::home_dir().unwrap();
-        let log_path = home_dir.join(".config/testing_ls_adapter/logs");
+        let log_path = home_dir.join(".config/testing_language_server/adapter/logs");
         let file_appender = tracing_appender::rolling::daily(log_path, "prefix.log");
         let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
         tracing_subscriber::fmt().with_writer(non_blocking).init();
