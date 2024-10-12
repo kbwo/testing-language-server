f6aea922648c8726073b695cec8909ff2f9cc8a4
kbwo <kabaaa1126@gmail.com>
2024-10-12 17:30:49 +0900

    feat(adapter): delete old adapter log files

--- crates/adapter/src/log.rs
+++ crates/adapter/src/log.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 
+use testing_language_server::util::clean_old_logs;
 use tracing_appender::non_blocking::WorkerGuard;
 
 pub struct Log;
@@ -12,9 +13,16 @@ impl Log {
     }
 
     pub fn init() -> Result<WorkerGuard, anyhow::Error> {
-        let log_path = Self::log_dir();
-        let file_appender = tracing_appender::rolling::daily(log_path, "prefix.log");
+        let log_dir_path = Self::log_dir();
+        let file_appender = tracing_appender::rolling::daily(&log_dir_path, "prefix.log");
         let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+        clean_old_logs(
+            log_dir_path.to_str().unwrap(),
+            30,
+            "prefix.log.*",
+            "prefix.log.",
+        )
+        .unwrap();
         tracing_subscriber::fmt().with_writer(non_blocking).init();
         Ok(guard)
     }
